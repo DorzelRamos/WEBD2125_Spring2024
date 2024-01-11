28f53372125306365d5b890168a241a2e6ae3b0e
DorzelRamos <dorzel.ramos@gmail.com>
2024-01-11 13:45:30 -0600

    another commit

--- js/app2.js
+++ js/app2.js
@@ -22,18 +22,22 @@ function getArtist(aaa) {
 let i;
 let items = aaa.responseXML;
 let output = "<tr><th>Artist</th><th>Album</th><th>Label</th><th>Year</th></tr>";
-let artistList;
 let x = items.getElementsByTagName('artist');
-let container = document.getElementById("container");
+// let container = document.getElementById("container");
 // let sections = document.getElementsByClassName("artist-sections");
 // consople.log(x.length)
 console.log(x);
 for(i = 0; i < x.length; i++) {
-    output += '<tr><td>' + x[i].getElementsByTagName('artistName')[0].childNodes[0].nodeValue + '</td>';
-    output += '<td>' + x[i].getElementsByTagName('album')[0].childNodes[0].nodeValue + '</td>';
-    output += '<td>' + x[i].getElementsByTagName('label')[0].childNodes[0].nodeValue + '</td>';
-    output += '<td>' + x[i].getElementsByTagName('year')[0].childNodes[0].nodeValue + '</td></tr>';
+    let artistList;
+    artistList = "<section><h1>Artist: " + x[i].getElementsByTagName('artistName')[0].childNodes[0].nodeValue + '</td>';
 }
+// for(i = 0; i < x.length; i++) {
+//     let artistList;
+//     output += '<tr><td>' + x[i].getElementsByTagName('artistName')[0].childNodes[0].nodeValue + '</td>';
+//     output += '<td>' + x[i].getElementsByTagName('album')[0].childNodes[0].nodeValue + '</td>';
+//     output += '<td>' + x[i].getElementsByTagName('label')[0].childNodes[0].nodeValue + '</td>';
+//     output += '<td>' + x[i].getElementsByTagName('year')[0].childNodes[0].nodeValue + '</td></tr>';
+// }
     // document.querySelector(".container").innerHTML = artistList;
 
 }
