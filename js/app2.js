345c845e375ae387daa8f48e6ddd37bcabe1aebb
DorzelRamos <dorzel.ramos@gmail.com>
2024-01-16 13:49:33 -0600

    continue from last time

--- js/app2.js
+++ js/app2.js
@@ -1,5 +1,5 @@
 let request;
-if(window.XMLDocument) {
+if(window.XMLHttpRequest) {
     request = new XMLHttpRequest();
 }
 else {
@@ -21,27 +21,62 @@ request.send();
 function getArtist(aaa) {
 let i;
 let items = aaa.responseXML;
-let output = "<tr><th>Artist</th><th>Album</th><th>Label</th><th>Year</th></tr>";
+let artistList = '';
 let x = items.getElementsByTagName('artist');
-// let container = document.getElementById("container");
-// let sections = document.getElementsByClassName("artist-sections");
+let container = document.getElementById("container");
 // consople.log(x.length)
 console.log(x);
+
 for(i = 0; i < x.length; i++) {
-    let artistList;
-    artistList = "<section><h1>Artist: " + x[i].getElementsByTagName('artistName')[0].childNodes[0].nodeValue + '</td>';
+    artistList += '<div class="artists" id="artist' + (i+1) + '">';
+    artistList += '<section><h1 class="artists-section">Artist: ' + x[i].getElementsByTagName('artistName')[0].childNodes[0].nodeValue + "</h1>";
+    artistList += "<h3>Album: " + x[i].getElementsByTagName('album')[0].childNodes[0].nodeValue + "</h3>";
+    artistList += "<h4>Label: " + x[i].getElementsByTagName('label')[0].childNodes[0].nodeValue + "</h4>";
+    artistList += "<h4>Year: " + x[i].getElementsByTagName('year')[0].childNodes[0].nodeValue + "</h4>";
+    artistList += '<h4>Artist Bio: </h4><section class="bio"><p> ' + x[i].getElementsByTagName('bio')[0].childNodes[0].nodeValue + '</p><img src="images/' + x[i].getElementsByTagName('images')[0].childNodes[0].nodeValue + '.jpeg" alt="' + x[i].getElementsByTagName('album')[0].childNodes[0].nodeValue + ' "Album Image"></section>';
+    artistList += "</section></div>";
+    // you can wrap a single quote around double quotes in javascript
+
+    // let artistDiv = document.getElementsByClassName("artists");
+    // console.log(artistDiv[i].items(0));
+    // artistDiv[i].items(0).style.backgroundColor = bg[i];
 }
-// for(i = 0; i < x.length; i++) {
-//     let artistList;
-//     output += '<tr><td>' + x[i].getElementsByTagName('artistName')[0].childNodes[0].nodeValue + '</td>';
-//     output += '<td>' + x[i].getElementsByTagName('album')[0].childNodes[0].nodeValue + '</td>';
-//     output += '<td>' + x[i].getElementsByTagName('label')[0].childNodes[0].nodeValue + '</td>';
-//     output += '<td>' + x[i].getElementsByTagName('year')[0].childNodes[0].nodeValue + '</td></tr>';
-// }
-    // document.querySelector(".container").innerHTML = artistList;
+container.innerHTML = artistList;
+let artistDiv = document.getElementsByClassName("artists");
+    for (let i = 0; i < artistDiv.length; i++) {
+        console.log(artistDiv[i]);
+        artistDiv[i].style.backgroundColor = bg[i];
+        artistDiv[i].style.backgroundImage = gradient[i];
+    }
+// console.log(artistDiv.item(0));
 
 }
-// for (i = 0; i < x.length; i++) {
-//     artistList = "<h1>Artist: " + x[i].getElementsByTagName('artistName')[0].childNodes[0].nodeValue + "</h1>";
-//     document.getElementsByClassName("container").innerHTML = artistList;
+
+let bg1 = "#3EECAC";
+let gradient1 = "linear-gradient(19deg, #3EECAC 0%, #EE74E1 100%)";
+
+let bg2 = "#FBDA61";
+let gradient2 = "linear-gradient(45deg, #FBDA61 0%, #FF5ACD 100%)";
+
+let bg3 = "#FFDEE9";
+let gradient3 = "linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%)";
+
+let bg4 = "#52ACFF";
+let gradient4 = "linear-gradient(180deg, #52ACFF 25%, #FFE32C 100%)";
+
+let bg = [bg1, bg2, bg3, bg4];
+let gradient = [gradient1, gradient2, gradient3, gradient4];
+
+// window.onload = ()=> {
+//     let artistDiv = document.getElementsByClassName("artists");
+
+//     console.log(artistDiv.items(0));
+
+//     // for (let i = 0; i < artistDiv.length; i++) {
+//     //     console.log(artistDiv[i].item(0));
+//     //     artistDiv[i].item(0).style.backgroundColor = bg[i];
+//     // }
 // }
+
+
+
