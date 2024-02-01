7bfbf18133e7ec8c60e0898b152a6ebe5ab7d7d3
DorzelRamos <dorzel.ramos@gmail.com>
2024-02-01 15:41:35 -0600

    movie api with javascript, jquery. week4

---
+++ week4/movieland/js/app.js
@@ -0,0 +1,65 @@
+"use strict";
+$(document).ready(function(){
+    // const form = $('#movieform');
+    // const search = $('#search');
+    // const movies = $('#movies');
+
+    const form = document.getElementById('movieform');
+    const search = document.getElementById('search');
+    const movies = document.getElementById('movies');
+
+    // console.log(form + "\n" + search + "\n" + movies);
+    // const API = "http://www.omdbapi.com/?s=star trek&apikey=4269fb3f";
+    const url = "http://www.omdbapi.com/?s=";
+    const apiKey = "&apikey=4269fb3f"
+    let httpRequest;
+    // makeRequest();
+    form.addEventListener('submit', makeRequest);
+
+    function makeRequest(e){
+        e.preventDefault();
+
+        let query = search.value.trim();
+        if(window.XMLHttpRequest) {
+            httpRequest = new XMLHttpRequest();
+        }
+
+        else {
+        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        httpRequest.onreadystatechange = responseMethod;
+        httpRequest.open('GET', url + query + apiKey);
+        httpRequest.send();
+    }
+    
+   function responseMethod() {
+    if(httpRequest.readyState === 4) {
+        if(httpRequest.status === 200) {
+            updateUISuccess(httpRequest.responseText);
+        }
+        else {
+            updateUIError();
+        }
+    }
+   }
+
+   function updateUISuccess(test) {
+    let response = JSON.parse(test);
+    console.log(response);
+    let output = "<ul>";
+    for(let i = 0; i < response.Search.length; i++) {
+        output += "<li>";
+        output += "<h2>" + response.Search[i].Title + "</h2>";
+        output += '<img src="' + response.Search[i].Poster + '" alt="' + response.Search[i].Title + '">';
+        output += '<h3>' + response.Search[i].Year + '</h3>';
+        output += '</li>'
+    }
+    output += '</ul>';
+    $(movies).html(output);
+   }
+
+   function updateUIError() {
+    movies.className = "hidden";
+   }
+  
+  });
